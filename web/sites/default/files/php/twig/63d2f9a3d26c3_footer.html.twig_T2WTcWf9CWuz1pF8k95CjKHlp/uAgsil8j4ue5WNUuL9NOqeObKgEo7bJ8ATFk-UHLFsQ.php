be2eabb12fb3a64036fa9df9637a01c2eeabffda
MKHARBECH <salah_ita@yahoo.fr>
2023-01-26 23:20:33 +0100

    creation projet drupal recette_atlas avec docker-compose

---
+++ web/sites/default/files/php/twig/63d2f9a3d26c3_footer.html.twig_T2WTcWf9CWuz1pF8k95CjKHlp/uAgsil8j4ue5WNUuL9NOqeObKgEo7bJ8ATFk-UHLFsQ.php
@@ -0,0 +1,96 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @panache/includes/footer.html.twig */
+class __TwigTemplate_5cf744bd21db763b192c8610142351bb extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"panache-copyright\">
+  <div class=\"container\">
+    <p>Copyright &copy; <a href=\"";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["front_page"] ?? null), 3, $this->source), "html", null, true);
+        echo "\">Panache</a> | A theme by <a href=\"https://www.drupal.org/u/hansa11\" alt=\"theme author Hansa\" target=\"_blank\">Hansa</a></p>
+  </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "@panache/includes/footer.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 3,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "@panache/includes/footer.html.twig", "/opt/drupal/web/themes/contrib/panache/templates/includes/footer.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 3);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/63d2f9a3d26c3_header-navigation.html.tw_Vdf3K09ASJJQvx4OAKNJeSl6R/KEuhgJHEtVywalqfgXRDhl9PzTgW4WVFJJ1ZUcwupA0.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @panache/includes/header-navigation.html.twig */
+class __TwigTemplate_eb17f727171fdfc2916e656c3b8fcd88 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"panache-header-menu\" id=\"panache-menu\">
+  <div class=\"container\">
+    <div class=\"row\">
+      <div class=\"col-lg-12\">
+        <div class=\"navbar-toggle\">
+          <div class=\"bar1\"></div>
+          <div class=\"bar2\"></div>
+          <div class=\"bar3\"></div>
+        </div>
+        ";
+        // line 10
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "header_navigation", [], "any", false, false, true, 10), 10, $this->source), "html", null, true);
+        echo "
+      </div>
+    </div>
+  </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "@panache/includes/header-navigation.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 10,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "@panache/includes/header-navigation.html.twig", "/opt/drupal/web/themes/contrib/panache/templates/includes/header-navigation.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 10);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
